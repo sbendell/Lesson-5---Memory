e1b0e432cdc758083b54c34ede104cb97146e9f9
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-03 14:47:58 +0100

    Copied code

--- Lesson 5 - Memory.cpp
+++ Lesson 5 - Memory.cpp
@@ -2,10 +2,33 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
-int main()
-{
-    return 0;
+int main() {
+
+	int a;
+
+	int* b;
+
+	a = 100;
+
+	b = new int;
+
+	cout << "The pointer b points to the memory address " << b << " located on the heap\n";
+	cout << "The value in memory location b is " << *b << "\n";
+
+	*b = a;
+
+	cout << "The value of stack variable a is " << a << "\n";
+	cout << "The 'heap' memory pointed to by b contains " << *b << "\n";
+
+	delete b;
+	b = NULL;
+
+	cin >> *b;
+
+	return 0;
 }
 
